b4311983a928996ddfe562538bb564669032783e
Lukas Wagner <lukas.wagner@student.hpi.de>
2018-06-02 23:09:39 +0200

    refs #3: added mollusc class

---
+++ src/mollusc.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <string>
+
+#include <QColor>
+
+struct Mollusc
+{
+    Mollusc(const std::string& data);
+
+    std::string m_image;
+    QColor m_color;
+    float m_rotation;
+    float m_ratio;
+    std::string m_originalImage;
+    std::string m_inventoryNumber;
+    std::string m_class;
+    std::string m_family;
+    std::string m_genus;
+    std::string m_species;
+    std::string m_scientificName;
+    std::string m_locality;
+    std::string m_date;
+    std::string m_area;
+    std::string m_province;
+    std::string m_country;
+    std::string m_subContinent;
+    std::string m_continent;
+};
